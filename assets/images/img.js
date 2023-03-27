655ee3ff5f75864771517fdce810e564a9b89f31
Tiendq12712 <112851659+Tiendq12712@users.noreply.github.com>
2023-03-27 15:24:11 +0700

    add images

---
+++ assets/images/img.js
@@ -0,0 +1,99 @@
+export const iconthoat12 = require('../icons/Iconthoat-12.png');
+export const iconchapnhan07 = require('../icons/Iconchapnhan-07.png');
+export const AkaGyuLayout07 = require('../images/Aka-Gyu-Layout-07.jpg');
+export const AkaGyuLayout08 = require('../images/Aka-Gyu-Layout-08.jpg');
+export const AkaGyuLayout09 = require('../images/Aka-Gyu-Layout-09.jpg');
+export const AkaGyuLayout10 = require('../images/Aka-Gyu-Layout-10.jpg');
+export const AkaGyuLayout11 = require('../images/Aka-Gyu-Layout-11.jpg');
+export const AkaGyuLayout14 = require('../images/Aka-Gyu-Layout-14.jpg');
+export const IconGiamMon12 = require('../icons/IconGiamMon-12.png');
+export const IconThemMon12 = require('../icons/IconThemMon-12.png');
+export const BtnXoaKH12 = require('../icons/BtnXoaKH-12.png');
+export const BtnXacNhan12 = require('../icons/BtnXacNhan-12.png');
+export const BtnXoaTien12 = require('../icons/BtnXoaTien-12.png');
+export const IconTienMat12 = require('../icons/1-12.png');
+export const IconChinhsach_GiamPhieu12 = require('../icons/4-12.png');
+export const IconNganhang12 = require('../icons/2-12.png');
+export const IconVIP12 = require('../icons/3-12.png');
+export const IconGiammn12 = require('../icons/6-12.png');
+export const IconTamtinh12 = require('../icons/7-12.png');
+export const IconInKetthuc12 = require ('../icons/8-12.png');
+export const BtnList = require ('../icons/BtnList.png');
+export const IconBtnXoasotien12 = require('../icons/IconBtnXoasotien-12.png');
+export const showlist12 = require('../icons/showlist-12.png');
+export const IconCheckVIP12 = require('../icons/IconCheckVIP-12.png');
+export const IconCheckVIP212 = require('../icons/IconCheckVIP2-12.png');
+export const IconLoc12 = require('../icons/IconLoc-12.png') ;
+export const IconThemMon = require('../icons/IconTangMenuCombo-12.png');
+export const IconGiamMon = require('../icons/IconGiamMenuCombo-12.png');
+export const IconListOrderCombo12 = require('../icons/IconListOrderCombo-12.png');
+export const IconChucNang13 = require('../icons/IconChucNang-13.png');
+export const tamgiac = require('../images/tamgiac.png')
+export const IconBack_07 = require('../icons/IconBack-07.png')
+export const Icon_TTphieu_13 = require('../icons/Icon_TTphieu_13.png')
+export const Icon_ThemMon_13 = require('../icons/Icon_ThemMon-13.png')
+export const Icon_Thanhtoan_13 = require('../icons/Icon_Thanhtoan-13.png')
+export const Icon_KTorder_13 = require('../icons/Icon_KTorder-13.png')
+export const Icon_Khachve_13 = require('../icons/Icon_Khachve-13.png')
+export const Icon_Ketban_13 = require('../icons/Icon_Ketban-13.png')
+export const Icon_in_13 = require('../icons/Icon_in-13.png')
+export const Icon_Ghepban_13 = require('../icons/Icon_Ghepban-13.png')
+export const Icon_DSphuluc_13 = require('../icons/Icon_DSphuluc-13.png')
+export const Icon_DSphieu_13 = require('../icons/Icon_DSphieu-13.png')
+export const Icon_Dongban_13 = require('../icons/Icon_Dongban-13.png')
+export const Icon_Chuyenban_13 = require('../icons/Icon_Chuyenban-13.png')
+export const Icon_Checkmon_13 = require('../icons/Icon_Checkmon-13.png')
+export const IconOrderMon_13 = require('../icons/IconOrderMon-13.png')
+export const iconCash = require('../icons/iconCash.png')
+
+
+
+export default {
+    iconthoat12,
+    iconchapnhan07,
+    AkaGyuLayout07,
+    AkaGyuLayout08,
+    AkaGyuLayout09,
+    AkaGyuLayout10,
+    AkaGyuLayout11,
+    AkaGyuLayout14,
+    IconGiamMon12,
+    IconThemMon12,
+    BtnXoaKH12,
+    BtnXacNhan12,
+    BtnXoaTien12,
+    IconTienMat12,
+    IconChinhsach_GiamPhieu12,
+    IconNganhang12,
+    IconVIP12,
+    IconGiammn12,
+    IconTamtinh12,
+    IconInKetthuc12,
+    BtnList,
+    IconBtnXoasotien12,
+    showlist12,
+    IconCheckVIP12,
+    IconCheckVIP212,
+    IconLoc12,
+    IconThemMon,
+    IconGiamMon,
+    IconListOrderCombo12,
+    IconChucNang13,
+    tamgiac,
+    IconBack_07,
+    Icon_TTphieu_13,
+    Icon_ThemMon_13,
+    Icon_Thanhtoan_13,
+    Icon_KTorder_13,
+    Icon_Khachve_13,
+    Icon_Ketban_13,
+    Icon_in_13,
+    Icon_Ghepban_13,
+    Icon_DSphuluc_13,
+    Icon_DSphieu_13,
+    Icon_Dongban_13,
+    Icon_Chuyenban_13,
+    Icon_Checkmon_13,
+    IconOrderMon_13,
+    iconCash
+};
